de308087c79875dca7581e32700b0a72983e7347
jaydip-valens <jaydip.makwana@valensdatalabs.com>
2024-10-03 15:04:37 +0530

    add keys

--- src/pages/result/SearchResult.jsx
+++ src/pages/result/SearchResult.jsx
@@ -4,7 +4,6 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import { useParams } from "react-router-dom";
 import { fetchDataFromApi } from "../../utils/api";
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
-import noResults from "../../assets/no-results.png";
 import Loader from "../../components/Loader/Loader";
 import Card from "../../components/Card/Card";
 import LazyImg from "../../components/lazyLoadImage/LazyImg";
